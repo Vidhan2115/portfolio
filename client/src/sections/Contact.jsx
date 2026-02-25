5cd5d0a0361ae00cf7383b1b8f109ba2e5bf5ab6
Vidhan2115 <192176457+Vidhan2115@users.noreply.github.com>
2026-02-26 01:51:38 +0530

    updated backend URL

--- client/src/sections/Contact.jsx
+++ client/src/sections/Contact.jsx
@@ -24,7 +24,7 @@ const Contact = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/contact",
+        " https://graphyt-backend.onrender.com/api/contact",
         formData
       );
 
